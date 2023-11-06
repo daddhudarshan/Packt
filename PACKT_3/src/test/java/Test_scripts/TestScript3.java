320e100cc525e47f53394611ab678824dce8635b
daddhudarshan <darshandevaraj10@gmail.com>
2023-11-06 14:53:01 +0530

    Third Test

--- PACKT_3/src/test/java/Test_scripts/TestScript3.java
+++ PACKT_3/src/test/java/Test_scripts/TestScript3.java
@@ -1,5 +1,31 @@
 package Test_scripts;
+import java.util.List;
 
-public class TestScript3 {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+public class TestScript3 extends BaseClass {
 
+	
+	
+	@Test
+	public void CarouselTest() throws InterruptedException {
+		 List<WebElement> ele = driver.findElements(By.xpath("//span[@class='align-self-end']"));
+		
+		 
+		for(WebElement list : ele){
+		        Thread.sleep(500);
+		        list.click();
+		
+		   String elementText = list.getText();
+		   String expectedText = "Causal Inference and Discovery in Python";
+		
+		   if (elementText.equals(expectedText)) {
+	            System.out.println("Validation passed: Element text is as expected.");
+	        } else {
+	            System.out.println("Validation failed: Element text does not match the expected text.");
+	        }
+		
+	}
+   }
 }
