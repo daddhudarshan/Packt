fe50a6c5d3a4e42f9ee963c43bbf7a42b9d95473
daddhudarshan <darshandevaraj10@gmail.com>
2023-11-06 12:14:07 +0530

    Added new Xpath

--- PACKT_3/src/main/java/packt_pom/home_page.java
+++ PACKT_3/src/main/java/packt_pom/home_page.java
@@ -1,4 +1,5 @@
 package packt_pom;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindAll;
@@ -6,85 +7,81 @@ import org.openqa.selenium.support.FindBy;
 
 public class home_page {
 
-	
 	public WebDriver driver;
-	
-	@FindBy(xpath="//a[@class='navbar-brand']")
-	public WebElement homebutton;
-	
-	
-	@FindBy(xpath="//a[(@data-rb-event-key='https://subscription.packtpub.com/search') and (@class='nav-link')]")
-	public WebElement browserlibrary;
-	
-	@FindBy(xpath="//button[@class='button--more-info btn btn-primary advanced-search-button']")
-	public WebElement advancesearch;
-	
-	@FindBy(xpath="//input[@class='mr-sm-2 form-control']")
-	public WebElement searchfield;
-	
 
-	@FindBy(xpath="//a[@id='library-dropdown']")
+	@FindBy(xpath = "//a[@class='navbar-brand']")
+	public WebElement homebutton;
+
+	@FindBy(xpath = "//a[(@data-rb-event-key='https://subscription.packtpub.com/search') and (@class='nav-link')]")
+	public WebElement browserlibrary;
+
+	@FindBy(xpath = "//button[@class='button--more-info btn btn-primary advanced-search-button']")
+	public WebElement advancesearch;
+
+	@FindBy(xpath = "//input[@class='mr-sm-2 form-control']")
+	public WebElement searchfield;
+
+	@FindBy(xpath = "//a[@id='library-dropdown']")
 	public WebElement librarydropdown;
-	
-	@FindBy(xpath="//a[@id='recent-dropdown']")
+
+	@FindBy(xpath = "//a[@id='recent-dropdown']")
 	public WebElement resentdropdown;
 
-	@FindBy(xpath="//div[@class='btn-content']")
+	@FindBy(xpath = "//div[@class='btn-content']")
 	public WebElement cartbutton;
-	
-	@FindBy(xpath="style-1 d-none d-lg-inline nav-link")
+
+	@FindBy(xpath = "style-1 d-none d-lg-inline nav-link")
 	public WebElement signoutbutton;
-	
-	
-	@FindBy(xpath="//a[@class='button']")
+
+	@FindBy(xpath = "//a[@class='button']")
 	public WebElement startfreetrail;
 
-	@FindBy(xpath="//a[@class='readNowButton btn-lg btn-block']")
+	@FindBy(xpath = "//a[@class='readNowButton btn-lg btn-block']")
 	public WebElement readnowbutton;
-	
-	@FindBy(xpath="//button[@type='button']")
+
+	@FindBy(xpath = "//button[@type='button']")
 	public WebElement fullnavbar;
-	
-	
+
 // method creation 
 
 	public void clkonhomebutton() {
 		homebutton.click();
 	}
-	
+
 	public void clkonbrowserlibrary() {
 		browserlibrary.click();
 	}
-	
+
 	public void clkonadvancesearch() {
 		advancesearch.click();
 	}
-	
-	
+
 	public void clkonsearchfield() {
 		searchfield.click();
 	}
-	
+
 	public void clkonlibrarydropdown() {
 		librarydropdown.click();
 	}
-	
+
 	public void clkonresentdropdown() {
-	  resentdropdown.click();
+		resentdropdown.click();
 	}
-	
+
 	public void clkoncartbutton() {
 		cartbutton.click();
 	}
+
 	public void clkonsignoutbutton() {
 		signoutbutton.click();
 	}
+
 	public void clkonstartfreetrail() {
 		startfreetrail.click();
 	}
-	
+
 	public void clkonreadnowbutton() {
 		readnowbutton.click();
 	}
-	
+
 }
